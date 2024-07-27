1830554f9ede7b79c1544d67bdfd2384855d74d9
Noctra <mnv2_dev@outlook.com>
2024-07-27 15:55:33 -0400

    Prettier

--- public/json-loader.js
+++ public/json-loader.js
@@ -1,31 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
 	if (window.location.pathname === '/g') {
-        fetch('g.json')
-            .then(response => response.json())
-            .then(data => {
-                const gameContainer = document.querySelector('.gameContain');
+		fetch('g.json')
+			.then(response => response.json())
+			.then(data => {
+				const gameContainer = document.querySelector('.gameContain');
 
-                data.sort((a, b) => a.name.localeCompare(b.name));
+				data.sort((a, b) => a.name.localeCompare(b.name));
 
-                data.forEach(game => {
-                    const gameLink = document.createElement('a');
-                    gameLink.href = `/&?q=${encodeURIComponent(game.name)}`;
-                    const gameImage = document.createElement('img');
-                    gameImage.src = game.img;
-                    gameImage.alt = game.name;
-                    gameImage.title = game.name;
-                    gameImage.className = 'gameImage';
+				data.forEach(game => {
+					const gameLink = document.createElement('a');
+					gameLink.href = `/&?q=${encodeURIComponent(game.name)}`;
+					const gameImage = document.createElement('img');
+					gameImage.src = game.img;
+					gameImage.alt = game.name;
+					gameImage.title = game.name;
+					gameImage.className = 'gameImage';
 
-                    gameImage.onerror = () => {
-                        gameImage.src = '/default.png';
-                    };
+					gameImage.onerror = () => {
+						gameImage.src = '/default.png';
+					};
 
-                    gameLink.appendChild(gameImage);
-                    gameContainer.appendChild(gameLink);
-                });
-            })
-            .catch(error => console.error('Error loading game :( ', error));
-    }
+					gameLink.appendChild(gameImage);
+					gameContainer.appendChild(gameLink);
+				});
+			})
+			.catch(error => console.error('Error loading game :( ', error));
+	}
 
 	if (window.location.pathname === '/&') {
 		fetch('shortcuts.json')
