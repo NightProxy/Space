5702ce4e7baba3be1921a9d75d9141cccc44cc82
Noctra <mnv2_dev@outlook.com>
2024-07-27 04:42:40 -0400

    Added Games and Fixed Bugs
    Co-authored-by: Crllect <crllect@users.noreply.github.com>

--- public/g.js
+++ public/g.js
@@ -4,12 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
 			.then(response => response.json())
 			.then(data => {
 				const gameContainer = document.querySelector('.gameContain');
+
+				data.sort((a, b) => a.name.localeCompare(b.name));
+
 				data.forEach(game => {
 					const gameLink = document.createElement('a');
 					gameLink.href = `/&?q=${encodeURIComponent(game.name)}`;
 					const gameImage = document.createElement('img');
 					gameImage.src = game.img;
 					gameImage.alt = game.name;
+					gameImage.title = game.name;
+
+					gameImage.onerror = () => {
+						gameImage.src = '/default.png';
+					};
+
 					gameLink.appendChild(gameImage);
 					gameContainer.appendChild(gameLink);
 				});
