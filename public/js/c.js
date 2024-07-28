714dafa2a9d1cc559e7410761ff3f7264f5e492d
crllect <crllect@proton.me>
2024-07-28 10:25:49 -0400

    prettier + c.js preface

--- public/js/c.js
+++ public/js/c.js
@@ -1,3 +1,10 @@
+/* This is basically the settings loader file, it loads the settings options,
+so like cloaking, panic key, etc. the search engine isnt set here because
+the code in index.js was already set. This isnt inside of index.js,
+because index.js has uv logic, and because uv isnt imported on every page,
+index.js fails to run on some pages. So c.js is its own import
+*/
+
 function isInLocalStorage(key) {
 	return localStorage.getItem(key) !== null;
 }
